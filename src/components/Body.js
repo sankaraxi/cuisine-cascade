57a1a9b2723193ee1112715502b96a4977256229
sankaraxi <sankarfrompalani@gmail.com>
2024-08-23 22:16:06 +0530

    freshup with hooks

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,151 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
+
 
 //not using keys(not acceptable) <<<<<< index as key <<<<<<<< unique id  (best practice) must follow
 const Body = () => {
+
+    // state variable is created using useState - super powerful variable 
+    // it maintains the state of your application
+    // a react hook is normal javascript function that starts with use
+    // javascript utility function given by react
+    // two important react hooks - useState, useEffect
+
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList); // local state variable 
+
+//     const [listOfRestaurants, setListOfRestaurants] = useState([
+//         {
+//         "info": {
+//           "id": "587665",
+//           "name": "Subway",
+//           "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/518837c9-252e-47e6-90d3-055f0ea2c79d_587665.JPG",
+//           "costForTwo": "₹350 for two",
+//           "cuisines": [
+//             "Salads",
+//             "Snacks",
+//             "Desserts",
+//             "Beverages"
+//           ],
+//           "avgRating": 3.7,
+//           "sla": {
+//             "deliveryTime": 20,
+//           },
+          
+//         }
+//         },
+//         {
+//     "info": {
+//       "id": "587666",
+//       "name": "My shop",
+//       "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/518837c9-252e-47e6-90d3-055f0ea2c79d_587665.JPG",
+//       "costForTwo": "₹350 for two",
+//       "cuisines": [
+//         "Salads",
+//         "Snacks",
+//         "Desserts",
+//         "Beverages"
+//       ],
+//       "avgRating": 4.7,
+//       "sla": {
+//         "deliveryTime": 20,
+//       },
+      
+//     }
+//         }, 
+//         {
+// "info": {
+//   "id": "587667",
+//   "name": "McDonalds",
+//   "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/518837c9-252e-47e6-90d3-055f0ea2c79d_587665.JPG",
+//   "costForTwo": "₹350 for two",
+//   "cuisines": [
+//     "Salads",
+//     "Snacks",
+//     "Desserts",
+//     "Beverages"
+//   ],
+//   "avgRating": 4.2,
+//   "sla": {
+//     "deliveryTime": 20,
+//   },
+  
+// }
+//         }
+//     ]);
+
+    // normal js variable
+//     let listOfRestaurantsJS = [
+//         {
+//             "info": {
+//               "id": "587665",
+//               "name": "Subway",
+//               "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/518837c9-252e-47e6-90d3-055f0ea2c79d_587665.JPG",
+//               "costForTwo": "₹350 for two",
+//               "cuisines": [
+//                 "Salads",
+//                 "Snacks",
+//                 "Desserts",
+//                 "Beverages"
+//               ],
+//               "avgRating": 3.7,
+//               "sla": {
+//                 "deliveryTime": 20,
+//               },
+              
+//             }
+//     },
+//     {
+//         "info": {
+//           "id": "587666",
+//           "name": "My shop",
+//           "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/518837c9-252e-47e6-90d3-055f0ea2c79d_587665.JPG",
+//           "costForTwo": "₹350 for two",
+//           "cuisines": [
+//             "Salads",
+//             "Snacks",
+//             "Desserts",
+//             "Beverages"
+//           ],
+//           "avgRating": 4.7,
+//           "sla": {
+//             "deliveryTime": 20,
+//           },
+          
+//         }
+// }, 
+// {
+//     "info": {
+//       "id": "587667",
+//       "name": "McDonalds",
+//       "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/518837c9-252e-47e6-90d3-055f0ea2c79d_587665.JPG",
+//       "costForTwo": "₹350 for two",
+//       "cuisines": [
+//         "Salads",
+//         "Snacks",
+//         "Desserts",
+//         "Beverages"
+//       ],
+//       "avgRating": 4.2,
+//       "sla": {
+//         "deliveryTime": 20,
+//       },
+      
+//     }
+// }]
+    
     return (
         <div className='body'>
-            <div className='search-bar' >
-                //search
+            <div className='filter'>
+                <button className="filter-btn" onClick={() => {
+                    const filteredRestaurants = listOfRestaurants.filter((restaurant) => restaurant.info.avgRating > 4.3);
+                    setListOfRestaurants(filteredRestaurants);
+                }}>Top Rated Restaurants</button>
+                
             </div>
             <div className='restau-container'>
-                {resList.map((restaurant) => (
+                {listOfRestaurants.map((restaurant) => (
                     <RestaurantCard key ={restaurant.info.id} resData={restaurant} />
                   ))} 
                 
