ba01924bb99426eb328cbb1cb762caaaf012838b
sankaraxi <sankarfrompalani@gmail.com>
2024-08-29 22:41:33 +0530

    fetched github userApi

--- src/components/About.js
+++ src/components/About.js
@@ -20,7 +20,7 @@ class About extends Component {
                 <h1>About Us</h1>
                 <h4>Welcome to Cuisine Cascade, your go-to online food delivery app. We are dedicated to bringing you a diverse range of culinary delights from the finest kitchens to your doorstep. Our commitment to quality and exceptional service ensures a seamless and satisfying experience, every time you order. Discover the taste of convenience with Cuisine Cascade.</h4>
                 <div className='card-container'>
-                    <User name="Sankar (function)"/>
+                    {/* <User name="Sankar (function)"/> */}
                     <UserClass name="Sankar (class)" location="Palani (class)"/>
                 </div>
             </div>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,29 +3,43 @@ import React from 'react';
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
-        console.log("UserClass constructor called");
 
         this.state = {
-            count: 0,
-            count2: 0,
+            userInfo: {
+                name: 'Dummy',
+                location: 'Default',
+            },
         };
     }
 
-    componentDidMount(){  
-        console.log("UserClass componentDidMount called");
+    async componentDidMount(){  
+        const data = await fetch('https://api.github.com/users/sankaraxi');
+        const jsonData = await data.json();
+
+        console.log(jsonData);
+        
+        this.setState(
+            {
+                userInfo: jsonData,
+            }
+        );
     }  
 
   render() {
-    console.log("UserClass render called");
-    const { name, location } = this.props;
-    const { count,count2 } = this.state;
-
+    const { name, location,avatar_url } = this.state.userInfo  ;
+    debugger; 
     return (
         <div className="user-card"> 
-            <h3>Name: {name}</h3>
-            <h4>Location: {location}</h4>
-            <h4>Contact: @sankaraxi</h4>
-            <h4>Count : {count} (class)</h4>
+            <img src={avatar_url} className='avatar' alt="avatar" />
+            <div className='user-info'>
+                <h3>Name: {name}</h3>
+                <h4>Location: {location}</h4>
+                <h4>Contact: @sankaraxi</h4>
+            </div>
+
+
+
+            {/* <h4>Count : {count} (class)</h4>
             <h4>count2 : {count2} (class)</h4>
             <button onClick={() => {
                 // Never update the state directly using this.state.count = this.state.count + 1;
@@ -34,7 +48,7 @@ class UserClass extends React.Component {
                     count: count + 1,
                     count2: count2 + 2 
                  });
-            }}>Increment</button>
+            }}>Increment</button> */}
 
         </div>
     );
