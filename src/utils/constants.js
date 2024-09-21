7d5a25364a517208b1f88c895963d08b7ee0cc04
sankaraxi <sankarfrompalani@gmail.com>
2024-09-21 23:58:51 +0530

    CORS resolved

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 // name export
 
-export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.084887052121998&lng=76.99808970093727&restaurantId=";
+// export const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.084887052121998&lng=76.99808970093727&restaurantId=";
+
+export const MENU_API = "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=11.084887052121998&lng=76.99808970093727&restaurantId=";
