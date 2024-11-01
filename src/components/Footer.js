ada33e6c4e3aa0bc2a2d68ed9a15cbc1f243deb1
sankaraxi <sankarfrompalani@gmail.com>
2024-11-01 23:38:38 +0530

    refactor Footer component for improved layout and styling consistency

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,13 @@ import { faXTwitter, faLinkedin, faMedium, faInstagram, faReddit } from '@fortaw
 const Footer = () => {
   return (
     <div className='bg-black sm:px-16 px-8 py-7'>
-        <div className='flex max-sm:justify-center'>
+        <div className='flex justify-center'>
             <Link href="/">
                 <img className='w-[190px]' src={'https://i.ibb.co/yWDNXn4/cusine-cascade-logo.png'} alt='logo' />
             </Link>
         </div>
 
-        <div className="mt-5 gap-3 flex flex-col text-white">
-            <div className="text-smsm:text-lg flex flex-col gap-2">
-                <p className="font-poppins "><span className="font-bold">Phone: </span>+91 0987654321</p>
-                <p className="font-poppins "><span className="font-bold">Email: </span>thescapadecafe@outlook.com</p>
-            </div>
+        <div className=" gap-3 flex flex-col text-white">
 
             <div className='flex gap-3 justify-center sm:mt-5 text-2xl'>
                     <Link to='https://x.com/sankar_axi' target="_blank">
@@ -38,7 +34,7 @@ const Footer = () => {
         </div>
 
         <div>
-            <p className='text-center text-white mt-5 text-lg' >© {new Date().getFullYear()} Cuisine Cascade. All rights reserved.</p>
+            <p className='text-center text-white mt-5 text-sm sm:text-lg' >© {new Date().getFullYear()} Cuisine Cascade. All rights reserved.</p>
         </div>
     </div>
   )
