4061ae4abccc62f93859dde152eff02468369aa0
sankaraxi <sankarfrompalani@gmail.com>
2024-11-01 15:54:56 +0530

    made cascademart page responsive

--- src/components/CascadeMart.js
+++ src/components/CascadeMart.js
@@ -1,12 +1,14 @@
 
 const CascadeMart = () => {
     return (
-        <div>
-            <h1>Welcome to our grocery store!!!</h1>
-            <h3>This store has many child components!</h3>
-            <h4>Created for dynamic bundling.</h4>
-            <h1></h1>
-            <h1>Operating Soon!!!</h1>
+        <div className="pt-[100px]">
+
+            <h1 className="text-2xl sm:text-4xl font-bold text-center sm:mt-3x  ">CascadeMart - Grocery Store</h1>
+            {/* <h3>This store has many child components!</h3>
+            <h4>Created for dynamic bundling.</h4> */}
+            <div className="flex flex-col justify-center items-center h-full">
+                <h1 className="text-center font-semibold text-lg">Coming Soon!!!</h1>
+            </div>
         </div>
     );
 }
