443cb0ce5e49d8961f584560b913e47570debf3e
sankaraxi <sankarfrompalani@gmail.com>
2024-11-01 17:38:24 +0530

    minor changes in cascademart page

--- src/components/CascadeMart.js
+++ src/components/CascadeMart.js
@@ -3,7 +3,7 @@ const CascadeMart = () => {
     return (
         <div className="pt-[100px]">
 
-            <h1 className="text-2xl sm:text-4xl font-bold text-center sm:mt-3x  ">CascadeMart - Grocery Store</h1>
+            <h1 className="text-2xl sm:text-4xl font-bold text-center sm:mt-3  ">CascadeMart - Grocery Store</h1>
             {/* <h3>This store has many child components!</h3>
             <h4>Created for dynamic bundling.</h4> */}
             <div className="flex flex-col justify-center items-center h-full">
